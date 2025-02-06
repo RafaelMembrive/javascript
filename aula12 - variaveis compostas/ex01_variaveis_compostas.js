e944a0fd1a79487a22a7c08a1f754d9a91264ec4
Rafael Membrive Costa <rafael.membrive@hotmail.com>
2025-02-06 19:43:14 -0300

    updates nos exercicios

--- aula12 - variaveis compostas/ex01_variaveis_compostas.js
+++ aula12 - variaveis compostas/ex01_variaveis_compostas.js
@@ -1,6 +1,7 @@
 let numeros = [7, 80, 9, 74, 5, 47, 58, 27, 82, 63, 39, 21, 67, 7, 37, 89, 59, 58, 39, 18]
 
-numeros.sort()
+
 for(let posicao in numeros){
+    numeros.sort()
     console.log(`A posição ${posicao} tem o valor ${numeros[posicao]}`)
 }
